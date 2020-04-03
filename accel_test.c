70f59880075675e5164d8740cdf1fd4fae1f0c96
Gentry Atkinson <gma23@txstate.edu>
2020-04-03 14:44:41 -0500

    added some skeleton to the c file

--- accel_test.c
+++ accel_test.c
@@ -5,3 +5,21 @@
 *Description: this will read a SparkFun LIS3DH 3-axis accelerometer and use the
   value of one axis to set... like an LED or something.
 ************************************************/
+
+#include <pololu/orangutan.h>
+
+//Connected pins on Baby Orangutan. Change to fit your setup
+#define headlight_pin IO_D7
+#define throttle_pin IO_B1
+#define steering_pin IO_B0
+#define ch3_pin IO_B2
+
+//For pulse_in
+#define LOW_PULSE   1
+#define HIGH_PULSE  2
+#define ANY_PULSE   3
+
+int main (){
+
+  return 0;
+}

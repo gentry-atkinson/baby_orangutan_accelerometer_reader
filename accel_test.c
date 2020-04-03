5163028d04965a95ef56e17cdbf3a648a1bb69fb
Gentry Atkinson <gma23@txstate.edu>
2020-04-03 14:41:46 -0500

    starting c file

---
+++ accel_test.c
@@ -0,0 +1,7 @@
+/************************************************
+*Author: Gentry Atkinson
+*Organization: Bobcat Robotics
+*Date: 03 Apr 2020
+*Description: this will read a SparkFun LIS3DH 3-axis accelerometer and use the
+  value of one axis to set... like an LED or something.
+************************************************/
